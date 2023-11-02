79eecf12e45f34164e1939ffa7f27dd9f3b8195a
abdillah attoumani <a.attoumani13@outlook.fr>
2023-11-02 18:00:15 +0100

    Doctor created

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,4 @@
 class Patient < ApplicationRecord
   # lets just keep it user, rather than complicating it by naming account
-  has_one user, as: :profile
+  has_one :user, as: :profile
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_164437) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_165427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "speciality"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "gender"
