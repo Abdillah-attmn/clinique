20d8f71f3417caf46bcaa697596cf14991b177bb
abdillah attoumani <a.attoumani13@outlook.fr>
2024-02-20 12:45:26 +0100

    Authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :phone_number, :profile])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :phone_number, :profile])
   end
 end
